e8bc3d210f1713351a9c8510820b1797e14f7d1f
sbscerti <ougene1203@gmail.com>
2024-08-09 21:25:29 +0900

    작업파일1

---
+++ academy/day01/day01/day01/main01.cpp
@@ -0,0 +1,23 @@
+#include<iostream> //
+
+using namespace std;
+
+void main() {
+
+	//printf("hello world! 가나다라마바사");
+	//printf("hello world! 가나다라마바사");
+	// 줄을 띄우려면 \n 은 enter의 효과를 가지고 있다.
+
+	// 실습: 애국가 1절 출력하기
+	printf("동해물과 백두산이 마르고 닳도록\n하느님이 보우하사 우리나라 만세");
+
+
+}
+
+//ctrl+F5
+
+// 기본적으로 프로그래밍은 항상 (어떤 언어, 프레임 워크든) 메인문을 실행합니다.
+
+// main 함수는 프로그래밍 시작점
+//#include<iostream> << [input, output] stream 을 다룬다
+//using namespace std << 여기서는 안쓰지만 앞으로는 계속 사용함
